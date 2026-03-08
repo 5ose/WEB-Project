a4fe4dd748ec8c8712921a07caf7ecc3db5491d3
Abdelrahmann-Abdelrassoul <s-abdelrahman.abdelrassoul@zewailcity.edu.eg>
2026-03-08 20:45:44 +0200

    Admin global Delete Permissions check added in ownershipMiddleware

--- backend/middleware/ownershipMiddleware.js
+++ backend/middleware/ownershipMiddleware.js
@@ -21,7 +21,30 @@ const checkOwnership = (getOwnerId) => {
   };
 };
 
-export default checkOwnership;
 
 // checkOwnership((req) => req.video.owner.toString())
 // checkOwnership((req) => req.review.user.toString())
+
+const checkOwnershipOrAdminDelete = (getOwnerId) => {
+  return (req, res, next) => {
+    const ownerId = getOwnerId(req);
+
+    if (!ownerId) {
+      return next(new AppError("Owner information could not be determined.", 500));
+    }
+
+    if (req.user.role === "admin") {
+      return next();
+    }
+
+    if (req.user._id.toString() !== ownerId.toString()) {
+      return next(
+        new AppError("You do not have permission to perform this action.", 403)
+      );
+    }
+
+    next();
+  };
+};
+
+export { checkOwnership, checkOwnershipOrAdminDelete };
